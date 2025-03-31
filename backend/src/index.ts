0409d562614aebe921c406ee9464199422c41912
Tenzin Pelletier <t2pellet@uwaterloo.ca>
2025-03-31 10:41:48 -0400

    feat: it starts now

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,10 @@
 import fastify, { FastifyInstance } from 'fastify';
-import pino from 'pino';
-import formBody from '@fastify/formbody';
 import cors from '@fastify/cors';
 import helmet from '@fastify/helmet';
 
 import jsonSchema from '@/schemas/json-schema.json';
 import loadConfig from '@/config/env.config';
-import { AppError, ERRORS } from '@/helpers/errors.helper';
+import { ERRORS } from '@/helpers/errors.helper';
 
 loadConfig();
 
@@ -14,26 +12,23 @@ const port = Number(process.env.API_PORT) || 5001;
 const host = String(process.env.API_HOST);
 let server: FastifyInstance;
 
-const startServer = async () => {
+function startServer() {
   server = fastify({
-    logger: pino({ level: process.env.LOG_LEVEL }),
+    logger: {
+      level: process.env.LOG_LEVEL,
+    },
   });
 
   // Register middlewares
-  server.register(formBody);
   server.register(cors);
   server.register(helmet);
 
   // Set validation schema
-  server.addSchema(jsonSchema);
+  server.addSchema({ $id: 'schema', ...jsonSchema });
 
   // Set error handler
   server.setErrorHandler((error, _request, reply) => {
     server.log.error(error);
-
-    if (error instanceof AppError) {
-      return reply.status(error.statusCode).send({ message: error.message });
-    }
 
     return reply
       .status(ERRORS.internalServerError.statusCode)
@@ -56,21 +51,16 @@ const startServer = async () => {
   });
 
   // Start server
-  try {
-    await server.listen({
-      port,
-      host,
-    });
-  } catch (err) {
+  server.listen({ port, host }).catch((err) => {
     server.log.error(err);
     process.exit(1);
-  }
-};
+  });
+}
+
+startServer();
 
 // Handle unhandled rejections
 process.on('unhandledRejection', (err) => {
   server.log.error('Unhandled Rejection', err);
   process.exit(1);
 });
-
-await startServer();
