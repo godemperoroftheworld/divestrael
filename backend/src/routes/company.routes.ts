a9dd6e71fd907cda4296a93b0da60ee00636dff3
Tenzin Pelletier <t2pellet@uwaterloo.ca>
2025-04-01 19:34:20 -0400

    feat: disable company post again

--- backend/src/routes/company.routes.ts
+++ backend/src/routes/company.routes.ts
@@ -1,16 +1,16 @@
 import { FastifyPluginCallback } from 'fastify';
 
 import companyController from '@/controllers/company.controller';
-import { companyGetParams, companyPostBody, companyResponse } from '@/schemas/company.schema';
+import { companyGetParams, companyResponse } from '@/schemas/company.schema';
 
 const companyRoutes: FastifyPluginCallback = async (fastify) => {
-  fastify.post(
-    '/',
-    {
-      schema: { body: companyPostBody, response: { 200: companyResponse } },
-    },
-    companyController.postCompanyHandler,
-  );
+  // fastify.post(
+  //   '/',
+  //   {
+  //     schema: { body: companyPostBody, response: { 200: companyResponse } },
+  //   },
+  //   companyController.postCompanyHandler,
+  // );
   // fastify.post(
   //   '/bulk',
   //   {
