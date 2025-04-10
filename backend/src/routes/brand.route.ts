857a075963a776d6a5dd88cc44e98b8e73e5823e
Tenzin Pelletier <t2pellet@uwaterloo.ca>
2025-04-10 01:39:59 -0400

    feat: fix up, new generated routes work now, something wrong with validation though

--- backend/src/routes/brand.route.ts
+++ backend/src/routes/brand.route.ts
@@ -1,52 +1,12 @@
-import { FastifyPluginAsyncZod } from 'fastify-type-provider-zod';
-import z from 'zod';
-
-import { idParams, searchQuery } from '@/schemas';
-import brandController from '@/controllers/brand.controller';
+import BrandController from '@/controllers/brand.controller';
 import { brandBody, brandResponse } from '@/schemas/brand.schema';
+import PrismaRoute from '@/routes/PrismaRoute';
+import { BrandWithCompany } from '@/services/brand.service';
 
-const brandRoutes: FastifyPluginAsyncZod = async (server) => {
-  server.post(
-    '/',
-    {
-      schema: {
-        body: brandBody,
-        response: { 200: brandResponse },
-      },
-    },
-    brandController.postBrand,
-  );
-  server.get(
-    '/:id',
-    {
-      schema: {
-        params: idParams,
-        response: { 200: brandResponse },
-      },
-    },
-    brandController.getBrand,
-  );
-  server.put(
-    '/:id',
-    {
-      schema: {
-        params: idParams,
-        body: brandBody.partial(),
-        response: { 200: brandResponse },
-      },
-    },
-    brandController.updateBrand,
-  );
-  server.get(
-    '/search',
-    {
-      schema: {
-        query: searchQuery,
-        response: { 200: z.array(brandResponse) },
-      },
-    },
-    brandController.searchBrand,
-  );
-};
+export default class BrandRoute extends PrismaRoute<'Brand', BrandWithCompany> {
+  public static readonly instance = new BrandRoute();
 
-export default brandRoutes;
+  private constructor() {
+    super('brand', BrandController.instance, brandBody, brandResponse);
+  }
+}
