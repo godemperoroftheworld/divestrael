386e6d1459ec1983e65d21c26a578880d2642ffd
Tenzin Pelletier <t2pellet@uwaterloo.ca>
2025-04-15 23:09:48 -0400

    feat: migrate company route and fix post

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -4,8 +4,8 @@ import { ZodTypeProvider } from 'fastify-type-provider-zod';
 import allInclusiveRoutes from '@/routes/allinclusive.route';
 import barcodeRoutes from '@/routes/barcode.route';
 import ProductRoutes from '@/routes/product.route';
-import companyRoutes from '@/routes/company.route';
 import BrandRoute from '@/routes/brand.route';
+import CompanyRoute from '@/routes/company.route';
 
 const routes: FastifyPluginAsync = async (server) => {
   const fastify = server.withTypeProvider<ZodTypeProvider>();
@@ -13,7 +13,7 @@ const routes: FastifyPluginAsync = async (server) => {
   fastify.register(barcodeRoutes, { prefix: '/barcode' });
   ProductRoutes.instance.register(fastify);
   BrandRoute.instance.register(fastify);
-  fastify.register(companyRoutes, { prefix: '/company' });
+  CompanyRoute.instance.register(fastify);
 };
 
 export default routes;
