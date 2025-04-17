e3dd2ed7ed3a75cbd846da29aa404b84dcc7c568
Tenzin Pelletier <t2pellet@uwaterloo.ca>
2025-04-16 22:04:51 -0400

    chore(fe): update prettier settings

--- frontend/src/components/providers.tsx
+++ frontend/src/components/providers.tsx
@@ -1,29 +1,35 @@
 'use client';
 import React from 'react';
-import { isServer, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+  isServer,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 
 function makeQueryClient() {
-    return new QueryClient({
-        defaultOptions: {
-            queries: {
-                staleTime: 60 * 1000,
-            },
-        },
-    });
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  });
 }
 
 let browserQueryClient: QueryClient | undefined = undefined;
 
 function getQueryClient() {
-    if (isServer) {
-        return makeQueryClient();
-    } else {
-        if (!browserQueryClient) browserQueryClient = makeQueryClient();
-        return browserQueryClient;
-    }
+  if (isServer) {
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
 }
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-    const queryClient = getQueryClient();
-    return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  const queryClient = getQueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
 }
