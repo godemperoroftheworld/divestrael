b98e0572d1da52e83282f3af6d345c80124b553f
Tenzin Pelletier <t2pellet@uwaterloo.ca>
2025-04-16 21:05:39 -0400

    feat: api rewrite in frontend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/about',
+        destination: process.env.DIVESTRAEL_BACKEND_URL as string,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
