7945453bc68e96cc0f184331dcc5aeb94825b307
Tenzin Pelletier <t2pellet@uwaterloo.ca>
2025-04-16 22:39:15 -0400

    feat(fe): api models

---
+++ frontend/src/types/api/product.ts
@@ -0,0 +1,13 @@
+import Brand from '@/types/api/brand';
+import Barcode from '@/types/api/barcode';
+import { Type } from 'class-transformer';
+
+export default class Product {
+  id!: string;
+  name!: string;
+  brandId!: string;
+  @Type(() => Brand)
+  brand?: Brand;
+  @Type(() => Barcode)
+  barcodes?: Barcode[];
+}
