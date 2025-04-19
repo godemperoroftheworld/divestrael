b8138bc1f116bbd9e6d7958e9283d54039df4de8
Tenzin Pelletier <t2pellet@uwaterloo.ca>
2025-04-19 17:20:24 -0400

    feat(fe): queries

--- frontend/src/utils/query.ts
+++ frontend/src/utils/query.ts
@@ -3,15 +3,32 @@ import { QueryParams, useDivestraelQuery } from '@/hooks/query';
 import { ClassConstructor } from 'class-transformer';
 import { ArrayElement } from '@/types/globals';
 import { AxiosRequestConfig } from 'axios';
+import { useMemo } from 'react';
 
-type QueryFunction<T extends object> = (
-  params?: QueryParams<T>,
+type QueryAllFunction<T extends object> = (
+  params?: QueryParams<ArrayElement<T>>,
 ) => UseQueryResult<T>;
 
-export function createQuery<T extends object>(
+type QueryOneFunction<T extends object> = (
+  id: string,
+  params?: Omit<QueryParams<ArrayElement<T>>, 'filter' | 'orderBy'>,
+) => UseQueryResult<T>;
+
+export function createAllQuery<T extends object>(
   model: ClassConstructor<ArrayElement<T>>,
   url: string,
   config: Omit<AxiosRequestConfig, 'url'> = {},
-): QueryFunction<T> {
+): QueryAllFunction<T> {
   return (params = {}) => useDivestraelQuery<T>(model, url, config, params);
+}
+
+export function createOneQuery<T extends object>(
+  model: ClassConstructor<ArrayElement<T>>,
+  url: string,
+  config: Omit<AxiosRequestConfig, 'url'> = {},
+): QueryOneFunction<T> {
+  return (id, params = {}) => {
+    const urlFixed = useMemo(() => `${url}/${id}`, [id]);
+    return useDivestraelQuery(model, urlFixed, config, params);
+  };
 }
