9ad932a4cf32cce6c43465db47887806d057ac7c
Tenzin Pelletier <t2pellet@uwaterloo.ca>
2025-05-29 13:32:10 -0400

    fix(fe): carousel styling

--- frontend/src/components/carousel/index.tsx
+++ frontend/src/components/carousel/index.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import useEmblaCarousel from 'embla-carousel-react';
 import { WheelGesturesPlugin } from 'embla-carousel-wheel-gestures';
 
 import './styles.module.css';
 
-interface CarouselProps {
+interface CarouselProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode[];
 }
 
-export default function Carousel({ children }: CarouselProps) {
+export default function Carousel({ children, className }: CarouselProps) {
   const [emblaRef] = useEmblaCarousel({ loop: true, skipSnaps: true }, [
     WheelGesturesPlugin(),
   ]);
 
+  const embaClass = useMemo(() => `embla ${className}`, [className]);
+
   return (
-    <div className="embla">
+    <div className={embaClass}>
       <div
         className="embla__viewport overflow-hidden"
         ref={emblaRef}>
-        <div className="embla__container flex">
+        <div className="embla__container flex gap-2">
           {children.map((child, idx) => (
             <div
               className="emba__slide shrink-0"
