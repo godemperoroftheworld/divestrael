6cfa4214cf0c21f19c100d2e5fba43494544a79b
Tenzin Pelletier <t2pellet@uwaterloo.ca>
2025-05-29 16:56:25 -0400

    feat(fe): further improve search

--- frontend/src/components/search/index.tsx
+++ frontend/src/components/search/index.tsx
@@ -1,10 +1,15 @@
 'use client';
 
 import { useSearchCompanies } from '@/services/company/queries';
-import { useMemo, useState } from 'react';
+import { useMemo, useState, useCallback } from 'react';
 import { useSearchBrands } from '@/services/brand/queries';
 import { useSearchProducts } from '@/services/product/queries';
-import Select from 'react-select';
+import dynamic from 'next/dynamic';
+import Brand from '@/types/api/brand';
+import Company from '@/types/api/company';
+import Product from '@/types/api/product';
+import { GetOptionLabel } from 'react-select';
+const Select = dynamic(() => import('react-select'), { ssr: false });
 
 export default function Search() {
   const [searchQuery, setSearchQuery] = useState<string>('');
@@ -13,38 +18,38 @@ export default function Search() {
   const { data: brandData } = useSearchBrands(searchQuery, { take: 5 });
   const { data: productData } = useSearchProducts(searchQuery, { take: 5 });
 
+  const filterOption = useCallback(() => true, []);
+  const getOptionLabel: GetOptionLabel<Brand | Company | Product> = useCallback(
+    (option) => option.name,
+    [],
+  );
   const searchOptions = useMemo(
     () => [
       {
-        label: 'Companies',
-        options: data ?? [],
+        label: 'Products',
+        options: productData ?? [],
       },
       {
         label: 'Brands',
         options: brandData ?? [],
       },
       {
-        label: 'Products',
-        options: productData ?? [],
+        label: 'Companies',
+        options: data ?? [],
       },
     ],
     [data, brandData, productData],
   );
 
-  const formatGroupLabel = (data: { label: string; options: unknown[] }) => (
-    <div className="flex align-center justify-between">
-      <span>{data.label}</span>
-      <span>{data.options.length}</span>
-    </div>
-  );
-
   return (
     <Select
-      options={searchOptions}
-      formatGroupLabel={formatGroupLabel}
-      getOptionLabel={(option) => option.name}
-      value={searchQuery}
+      id="select"
+      options={typeof window !== undefined ? searchOptions : []}
+      getOptionLabel={getOptionLabel}
+      inputValue={searchQuery}
+      filterOption={filterOption}
       onInputChange={setSearchQuery}
+      maxMenuHeight={5000}
     />
   );
 }
