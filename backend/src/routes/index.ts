678f4204e74beb6ac00dfe2c371a9d4704ad00f4
Tenzin Pelletier <t2pellet@uwaterloo.ca>
2025-07-23 23:41:46 -0400

    feat(be)!: remove barcode routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -5,12 +5,10 @@ import allInclusiveRoutes from '@/routes/allinclusive.route';
 import ProductRoutes from '@/routes/product.route';
 import BrandRoute from '@/routes/brand.route';
 import CompanyRoute from '@/routes/company.route';
-import BarcodeRoute from '@/routes/barcode.route';
 
 const routes: FastifyPluginAsync = async (server) => {
   const fastify = server.withTypeProvider<ZodTypeProvider>();
   fastify.register(allInclusiveRoutes, { prefix: '/allinclusive' });
-  BarcodeRoute.instance.register(fastify);
   ProductRoutes.instance.register(fastify);
   BrandRoute.instance.register(fastify);
   CompanyRoute.instance.register(fastify);
