d13e2aaacd6cbc3e185887ab438c009d7daf3dd5
Tenzin Pelletier <t2pellet@uwaterloo.ca>
2025-07-25 02:25:00 -0400

    feat(fe): human-friendly names of boycott reasons

--- frontend/src/types/api/company.ts
+++ frontend/src/types/api/company.ts
@@ -1,13 +1,18 @@
 import Country from '@/types/api/country';
 import BoycottReason from '@/types/api/reason';
 import Brand from '@/types/api/brand';
-import { Type } from 'class-transformer';
+import { Transform, Type } from 'class-transformer';
 
 export default class Company {
   public id!: string;
   public name!: string;
   public description!: string;
   public country!: Country;
+  @Transform(
+    ({ value }) =>
+      value.map((v: string) => BoycottReason[v as keyof typeof BoycottReason]),
+    { toClassOnly: true },
+  )
   public reasons!: BoycottReason[];
   public cik!: number | null;
   public cw_id!: number | null;
