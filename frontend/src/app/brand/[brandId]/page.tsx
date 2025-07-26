5586ee06332b5b9bb064f291dfdca17b865b5100
Tenzin Pelletier <t2pellet@uwaterloo.ca>
2025-07-26 02:20:16 -0400

    feat(fe): metadata for brand/company pages

--- frontend/src/app/brand/[brandId]/page.tsx
+++ frontend/src/app/brand/[brandId]/page.tsx
@@ -2,6 +2,7 @@ import { prefetchBrand, prefetchBrands } from '@/services/brand/queries';
 import Client from './client';
 import getQueryClient from '@/services/query';
 import Hydrater from '@/components/hydrater';
+import { Metadata } from 'next';
 
 interface Props {
   params: Promise<{
@@ -10,6 +11,16 @@ interface Props {
 }
 
 const queryClient = getQueryClient();
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { brandId } = await params;
+  const brand = await prefetchBrand(queryClient, brandId);
+
+  return {
+    title: brand?.name,
+    description: `Should I boycott ${brand?.name}?`,
+  };
+}
 
 export async function generateStaticParams() {
   const brands = await prefetchBrands(queryClient, { select: ['id'] });
--- frontend/src/app/company/[companyId]/page.tsx
+++ frontend/src/app/company/[companyId]/page.tsx
@@ -1,7 +1,8 @@
 import { use } from 'react';
 import CompanyInfo from '@/components/company/company-info';
-import { prefetchCompanies } from '@/services/company/queries';
+import { prefetchCompanies, prefetchCompany } from '@/services/company/queries';
 import getQueryClient from '@/services/query';
+import { Metadata } from 'next';
 
 interface Props {
   params: Promise<{
@@ -9,8 +10,19 @@ interface Props {
   }>;
 }
 
+const queryClient = getQueryClient();
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { companyId } = await params;
+  const company = await prefetchCompany(queryClient, companyId);
+
+  return {
+    title: company?.name,
+    description: `Should I boycott ${company?.name}?`,
+  };
+}
+
 export async function generateStaticParams() {
-  const queryClient = getQueryClient();
   const companies = await prefetchCompanies(queryClient, { select: ['id'] });
   return companies.map((c) => ({ companyId: c.id }));
 }
