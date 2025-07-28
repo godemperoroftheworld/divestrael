afd368644faefeb2680ad5e3a3c2b4a3ecb32bdc
Tenzin Pelletier <t2pellet@uwaterloo.ca>
2025-07-28 14:36:09 -0400

    feat(fe): skeleton loader for camera

--- frontend/src/components/snap/product-camera.tsx
+++ frontend/src/components/snap/product-camera.tsx
@@ -1,18 +1,47 @@
 'use client';
 
 import Camera from 'react-webcam';
-import { HTMLAttributes, forwardRef, ForwardedRef } from 'react';
+import {
+  HTMLAttributes,
+  forwardRef,
+  ForwardedRef,
+  useState,
+  useCallback,
+} from 'react';
+import { mergeClasses } from '@/utils/class';
 function ProductCamera(
-  props: HTMLAttributes<HTMLDivElement>,
+  { className, ...props }: HTMLAttributes<HTMLDivElement>,
   ref: ForwardedRef<Camera | null>,
 ) {
+  const [isInitialised, setIsInitialised] = useState(false);
+
+  // 50ms delay otherwise it like flickers
+  const onInit = useCallback(
+    () =>
+      setTimeout(() => {
+        setIsInitialised(true);
+      }, 100),
+    [],
+  );
+
   return (
-    <div {...props}>
+    <div
+      className={mergeClasses(
+        className,
+        'bg-gray-500',
+        isInitialised ? undefined : 'animate-pulse',
+      )}
+      {...props}>
       <Camera
         ref={ref}
+        className={mergeClasses(
+          'w-xl max-w-full',
+          isInitialised ? undefined : 'max-md:aspect-[3/4] md:aspect-[4/3]',
+        )}
         videoConstraints={{ facingMode: 'environment' }}
         screenshotFormat="image/png"
         disablePictureInPicture={true}
+        onUserMedia={onInit}
       />
     </div>
   );
