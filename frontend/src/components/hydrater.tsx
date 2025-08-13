ec67513c72ef730e0f2447431554ba9e3880bb64
Tenzin Pelletier <t2pellet@uwaterloo.ca>
2025-08-13 12:46:50 -0400

    fix(fe): compile

--- frontend/src/components/hydrater.tsx
+++ frontend/src/components/hydrater.tsx
@@ -5,12 +5,13 @@ import {
 } from '@tanstack/react-query';
 import { instanceToPlain } from 'class-transformer';
 import { PropsWithChildren } from 'react';
+import { DehydratedState } from '@tanstack/query-core';
 
 type Props = PropsWithChildren<{
   queryClient: QueryClient;
 }>;
 export default function Hydrater({ queryClient, children }: Props) {
-  const dehydrated = instanceToPlain(dehydrate(queryClient));
+  const dehydrated = instanceToPlain(dehydrate(queryClient)) as DehydratedState;
 
   return <HydrationBoundary state={dehydrated}>{children}</HydrationBoundary>;
 }
